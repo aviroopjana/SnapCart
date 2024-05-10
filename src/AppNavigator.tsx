a8c8092d1a200ce94b0d9afb9174d15aca28bc7b
aviroopjana <aviroopjana@gmail.com>
2024-05-10 09:35:58 +0530

    feat: add Login and Signup screens to AppNavigator

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Splash from "./screens/Splash";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
 
 const AppNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -13,6 +15,16 @@ const AppNavigator = () => {
           options={{ headerShown: false }}
           name="Splash"
           component={Splash}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Signup"
+          component={Signup}
         />
       </Stack.Navigator>
     </NavigationContainer>
