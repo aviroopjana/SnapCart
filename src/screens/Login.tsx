bdd830b3ac6a9b9b612c853be5327c85536bc5ae
aviroopjana <aviroopjana@gmail.com>
2024-05-17 23:58:58 +0530

    refactor: Update Login screen UI and add email validation

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,35 @@
-import { View, Image, Text, TextInput, Pressable } from "react-native";
-import React from "react";
+import {
+  View,
+  Image,
+  Text,
+  TextInput,
+  Pressable,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from "react-native";
+import React, { useState } from "react";
 import LinearGradient from "react-native-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
+import Feather from 'react-native-vector-icons/Feather';
+import Error from 'react-native-vector-icons/MaterialIcons';
 
 const Login = () => {
   const navigation = useNavigation();
+
+  const [email, setEmail] = useState<String | undefined>();
+  const [verifyEmail, setVerifyEmail] = useState<boolean>();
+  const [password, setPassword] = useState<String>();
+  const [verifyPassword, setVerifyPassword] = useState<boolean>();
+
+  const handleEmail = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    const emailVar = e.nativeEvent.text;
+    setEmail(emailVar);
+    setVerifyEmail(false);
+    if (/^[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,}$/.test(emailVar)) {
+      setEmail(emailVar);
+      setVerifyEmail(true);
+    }
+  };
 
   return (
     <LinearGradient
@@ -26,9 +51,18 @@ const Login = () => {
           />
           <TextInput
             placeholder="Enter Email Id"
-            className="h-10 pl-[54px] bg-transparent w-full"
+            className="h-10 pl-[54px] bg-transparent w-full "
+            onChange={(e) => handleEmail(e)}
           />
+          {email?.length < 1 ? null : verifyEmail ? (
+            <Feather name="check_circle" color="green" size={20} />
+          ): ( <Error name="error" color="red" size={20} />)}
         </View>
+        {email?.length < 1 ? null : verifyEmail ? null : (
+            <Text className="ml-[20px] mt-1 text-red-700">
+              Enter Proper Email Address
+            </Text>
+          )}
         <View className="flex flex-row items-center bg-slate-200 rounded-xl mt-4 w-10/12">
           <Image
             source={require("../images/lock.png")}
@@ -48,7 +82,7 @@ const Login = () => {
         <Text
           className="mt-4 text-sm text-[#032457] font-medium"
           onPress={() => {
-            navigation.navigate('Signup' as never);
+            navigation.navigate("Signup" as never);
           }}
         >
           Create New Account?
